ba65f5140c98941dfeeb419c6e42d1f0dd0d8759
Steve Kane <quantumkane00@gmail.com>
2021-03-15 13:13:08 -0500

    added get_zillow_data

--- wrangle.py
+++ wrangle.py
@@ -68,3 +68,17 @@ def wrangle_telco():
     '''
     df = clean_telco(get_telco_data())
     return split(df)
+
+def get_zillow_data():
+    '''
+    This function uses the SQL query from below and specifies the database to use
+    '''
+    # SQL query that joins all of the tables together from the 'telco_churn' database     
+    sql_query = """
+                SELECT * 
+                FROM properties_2017
+                JOIN predictions_2017 USING(parcelid)
+                WHERE transactiondate BETWEEN "2017-05-01" AND "2017-06-30"
+                LIMIT 5000;
+                """
+    return pd.read_sql(sql_query,get_connection('zillow'))
