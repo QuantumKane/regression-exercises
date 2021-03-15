810b1e3c5db08fc05854d3382038d258f9236b21
Steve Kane <quantumkane00@gmail.com>
2021-03-15 14:18:42 -0500

    initial commit

---
+++ explore.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings("ignore")
+
+import env
+import wrangle
