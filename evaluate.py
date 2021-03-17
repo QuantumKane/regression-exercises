640c9f2542a171d1ec1bd62662a75ce3dee21f26
Steve Kane <quantumkane00@gmail.com>
2021-03-16 19:50:29 -0500

    finished

---
+++ evaluate.py
@@ -0,0 +1,51 @@
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return math.sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'SSE': sse(actual, predicted),
+        'MSE': mse(actual, predicted),
+        'RMSE': rmse(actual, predicted),
+        'ESS': ess(actual, predicted),
+        'TSS': tss(actual),
+    })
+
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'SSE': sse(actual, predicted),
+        'MSE': mse(actual, predicted),
+        'RMSE': rmse(actual, predicted),
+    }
+
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline
+
+
+def model_significance(ols_model):
+    return {
+        'r^2 -- variance explained': ols_model.rsquared,
+        'p-value -- P(data|model == baseline)': ols_model.f_pvalue,
+    }
