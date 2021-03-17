56b196741e3e086af3a4f30db07c1a8b83cc4973
Steve Kane <quantumkane00@gmail.com>
2021-03-16 21:05:03 -0500

    added clean_zillow and remove_outliers

--- wrangle.py
+++ wrangle.py
@@ -79,6 +79,49 @@ def get_zillow_data():
                 FROM properties_2017
                 JOIN predictions_2017 USING(parcelid)
                 WHERE transactiondate BETWEEN "2017-05-01" AND "2017-06-30"
+                AND propertylandusetypeid = 261
                 LIMIT 5000;
                 """
     return pd.read_sql(sql_query,get_connection('zillow'))
+
+
+def clean_zillow(df):
+    '''
+    This function takes in a dataframe, and performs the following:
+    - renames columns to make them understandable
+    - sets parcelid as the index
+    - drops null/NAN rows
+    - removes outliers from tax_value and square_feet
+    '''
+    df = df.rename(columns={"bedroomcnt": "bedrooms", "bathroomcnt": "bathrooms", "calculatedfinishedsquarefeet": "square_feet", 
+                        "taxamount": "taxes", "regionidzip": "zip_code", "taxvaluedollarcnt": "tax_value", 
+                        "yearbuilt": "year_built", "regionidcounty": "county"})
+    df = df.set_index("parcel_id")
+    df = df.dropna()
+    
+    upper_bound, lower_bound = remove_outlier(df, "tax_value")
+    df = df[df.tax_value < upper_bound]
+    
+    upper_bound, lower_bound = remove_outlier(df, "square_feet")
+    df = df[df.square_feet < upper_bound]
+    
+    return df
+
+
+def remove_outlier(df, feature):
+    '''
+    This function takes in a dataframe's features and performs the following:
+    - calculates its 1st and 3rd quartiles
+    - uses their diference to calculate the IQR
+    - uses the IQR to determine upper and lower bounds
+    '''
+    
+    q1 = df[feature].quantile(.25)
+    q3 = df[feature].quantile(.75)
+    
+    iqr = q3 - q1
+    
+    upper_bound = q3 + (1.5 * iqr)
+    lower_bound = q1 - (1.5 * iqr)
+    
+    return upper_bound, lower_bound
