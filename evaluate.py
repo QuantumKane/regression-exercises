35c8d1dba76a8a31c2557d688dd6fd7de68de3d8
Steve Kane <quantumkane00@gmail.com>
2021-03-16 20:05:39 -0500

    complete again

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,19 @@
+# imports 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.formula.api import ols
+
+from pydataset import data
+from scipy import stats
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import f_regression 
+from math import sqrt
+import math
+
 
 def residuals(actual, predicted):
     return actual - predicted
